c4d14b1ceb1d56ea67cc1e278d0cd0961a00f7f0
Aleksandr Sobolev <prospekt-s@list.ru>
2020-07-02 23:00:23 +0300

    lesson7

--- lesson7/script.js
+++ lesson7/script.js
@@ -5,28 +5,69 @@ let isNumber = function (n) {
 };
 
 let money,
-    income = 'Фриланс',
-    //addExpenses = prompt('Перечислите возможные расходы, через запятую','Кино, спорт, коммуналка, бензин'),
-    //deposit = confirm('Есть ли у вас депозит в банке?'),
-    mission = 1000000, //Копим
-    period = 6; //Период
+    start = function () {
+        do {
+            money = prompt('Ваш месячный доход?', '');
+        }
+        while (!isNumber(money));
+    };
+start();
+
+
+
+let appData = {
+    budget: money,
+    budgetDay: 0,
+    budgetMonth: 0,
+    expensesMonth: 0,
+    income: {},
+    addIncome: [],
+    expenses: {},
+    addExpenses: [],
+    deposit: false,
+    mission: 100000,
+    period: 6,
+    asking: function(){
+        let addExpenses = prompt('Перечислите возможные расходы, через запятую','Кино, спорт, коммуналка, бензин');
+            appData.addExpenses = addExpenses.toLowerCase().split(', ');
+            appData.deposit = confirm('Есть ли у вас депозит в банке?');
+
+
+            let amounts = [];
+
+
+            let sum = 0;
+    for (let i = 0; i < 2; i++) {
+
+        amounts[i] = prompt('Введите обязательную статью расходов?');
+        let checkPrompt = prompt('Во сколько это обойдется?');
+
+        while (!isNumber(checkPrompt)) {
+            checkPrompt = prompt('Во сколько это обойдется?');
+        }
+        sum += +checkPrompt;
+    }
+    return sum;
+    }
+};
+
+console.log(appData);
+
+
 
 // let expenses1 = prompt('Введите обязательную статью расходов?', 'садик'),
 //     amount1 = +prompt('Во сколько это обойдется?'),
 //     expenses2 = prompt('Введите обязательную статью расходов?', 'Бензин'),
 //     amount2 = +prompt('Во сколько это обойдется?'),
-let amounts = [];
 
-let start = function () {
-    do {
-        money = prompt('Ваш месячный доход?', '');
-    }
-    while (!isNumber(money));
-};
-start();
+//let amounts = [];
 
 
-const getExpensesMonth = function () {
+/*const showTypeOf = function (data) {
+   return typeof data;
+};*/
+
+/*appData.getExpensesMonth = function () {
     let sum = 0;
     for (let i = 0; i < 2; i++) {
 
@@ -39,60 +80,44 @@ const getExpensesMonth = function () {
         sum += +checkPrompt;
     }
     return sum;
-};
+    } 
+};*/
 
-let appData = {
-    budget: money,
-    budgetDay:0,
-    budgetMonth:0,
-    expensesMonth:0,
-};
-console.log(appData);
+/*let expensesAmout = appData.getExpensesMonth();
 
-let addExpenses;
-          addExpenses = prompt('Перечислите возможные расходы, через запятую','Кино, спорт, коммуналка, бензин');
-          appData.addExpenses = addExpenses.toLowerCase().split(', ');
-          appData.deposit = confirm('Есть ли у вас депозит в банке?');
-          console.log('возможные расходы за месяц: ', addExpenses.toLowerCase().split(', '));
-
-
-let expensesAmout = getExpensesMonth();
-
-appData.getExpensesMonth = function() {
-    console.log('Сумма всех обязательных расходов за месяц: ', expensesAmout);
-};
-appData.getExpensesMonth();
-
-
-
-
-appData.getAccumulatedMonth = function (a, b) {
+ appData.getAccumulatedMonth = function (a, b) {
     return a - b;
-};
-appData.getAccumulatedMonth();
+};*/
 
-let accumulatedMonth = appData.getAccumulatedMonth(money, expensesAmout);
+appData.getExpensesMonth = function (){
+    for (let key in appData.expenses) {
+        appData.expensesMonth += +appData.expenses[key];
+     }
+     };
+     
+
+     appData.Budget = function(){
+        appData.budgetMonth = appData.budget - appData.expensesMonth; 
+         appData.budgetDay = Math.floor(appData.budgetMonth / 30);
+           
+         },
+     appData.TargetMonth = function(){
+         return appData.mission / appData.budgetMonth;
+             
+     },
 
 
-// Функция расчета срока достижения цели
-appData.getTargetMonth = function (a, b) {
-    let splitNumber = Math.ceil(a / b);
-    if (splitNumber < 0) {
-        return `Цель не будет достигнута! Расходы превышают доходы!`;
-    } else {
-        return `Cрок достижения цели, в месяцах: ${splitNumber}`;
-    }
-};
-console.log(appData.getTargetMonth(mission, accumulatedMonth));
-appData.getTargetMonth();
+
+
+     appData.asking();
+     appData.Budget();
+     appData.getExpensesMonth();
 
 
 
-let budgetDay = Math.floor(accumulatedMonth / 30);
-appData.budgetDay = function() {
-    console.log('Бюджет на день, в рублях: ', budgetDay);
-};
-appData.budgetDay();
+//let accumulatedMonth = appData.getAccumulatedMonth(money, expensesAmout);
+//appData.budgetDay = Math.floor(accumulatedMonth / 30);
+
 
 
 appData.getStatusIncome = function (a) {
@@ -108,14 +133,40 @@ appData.getStatusIncome = function (a) {
         return 'Что-то пошло не так...';
     }
 };
-console.log('Статус Вашего дохода: ', appData.getStatusIncome(budgetDay));
-appData.getStatusIncome();
 
 
+let allinformation = function () {
+    for(let key in appData) {
+      console.log(
+        "Наша программа включает в себя данные:",
+        key,
+        appData[key]
+      );
 
+      }
+
+      allinformation();
+//console.log(money, showTypeOf(money));
+//console.log(appData.income, showTypeOf(appData.income));
+//console.log(appData.deposit, showTypeOf(appData.deposit));
 //console.log('Сумма всех обязательных расходов за месяц: ', expensesAmout);
-//console.log('возможные расходы за месяц: ', addExpenses.toLowerCase().split(', '));
-//console.log(getTargetMonth(mission, accumulatedMonth));
-//console.log('Бюджет на день, в рублях: ', budgetDay);
-//console.log('Статус Вашего дохода: ', getStatusIncome(budgetDay));
+//console.log(appData.getTargetMonth(appData.mission, accumulatedMonth));
 
+
+console.log('Бюджет на день, в рублях: ', appData.budgetDay);
+console.log('Статус Вашего дохода: ', appData.getStatusIncome(appData.budgetDay));
+
+
+
+
+// Функция расчета срока достижения цели
+appData.getTargetMonth = function (a, b) {
+    let splitNumber = Math.ceil(a / b);
+    if (splitNumber < 0) {
+        return `Цель не будет достигнута! Расходы превышают доходы!`;
+    } else {
+        return `Cрок достижения цели, в месяцах: ${splitNumber}`;
+    }
+};
+
+};
